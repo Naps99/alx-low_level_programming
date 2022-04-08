50d01cf70c3b48d2ab75d6d821533307a9ba4fdc
Naps99 <napsezi@gmail.com>
2022-04-08 12:07:51 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
-* main - prints is posetive, negative
-* or zero
-*Return: returns zero if program exits well.
+ * main 
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
-char c = 'a';
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
-putchar('\n');
-return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+
 }
