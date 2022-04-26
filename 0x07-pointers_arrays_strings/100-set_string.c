c65943eae3f3e811a0740c7382dbe6ec3343e648
Naps99 <napsezi@gmail.com>
2022-04-26 14:07:52 -0700

    Double pointer

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h> 
+
 /**
  * set_string - sets the value of a pointer to a char
  * @s: content is to be copied
- *@to: string
- *Return: void
+ * @to: string
+ * Return: void
  */
 
 void set_string(char **s, char *to)
