98f69d0d56f119a8687bc7209dcd4c17087f15f3
Naps99 <napsezi@gmail.com>
2022-04-26 13:48:07 -0700

    My Chess Task

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,15 +7,20 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	/Declaring variables/
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	i = 0;
+	while (i < 8) /number repetitions/
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
 		{
-			_putchar(a[i][j]);
+			_putchar (a[i][j]);
+			j++;
 		}
-
-	_putchar('\n');
+		_putchar ('\n'); /print new line/
+		i++; /add +1/
 	}
+
 }
