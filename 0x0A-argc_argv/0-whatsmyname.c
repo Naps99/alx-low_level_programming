18593cc2ad838031546a9143cc7ad123ae5e3e3e
Naps99 <napsezi@gmail.com>
2022-05-02 13:38:51 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - is a function that prints the name.
- *@argc: is an argument counter for char.
- *@argv: is an agument value for char.
- *Return: Returns always success.
-**/
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
-int i = argc - argc;
-
-printf("%s\n", argv[i]);
+(void) argc;
+printf("%s\n", argv[0]);
 
 return (0);
 }
-âˆ
