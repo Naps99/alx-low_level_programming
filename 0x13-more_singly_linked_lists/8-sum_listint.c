e347838c37b6eb49a8b123ca829e4b7c8a597261
Naps99 <napsezi@gmail.com>
2022-05-24 18:05:08 -0700

    Sum of all data(n)

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,45 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
- *
- * Return: pointer to the new node, or NULL
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+ * sum_listint - Sum all of the data (n) in a list.
+ * @head: Address of the first node of the list.
+ * Return: Integer.
+ **/
+
+int sum_listint(listint_t *head)
 {
-    unsigned int i;
-    listint_t *new;
-    listint_t *temp = *head;
+	int total = 0;
 
-    new = malloc(sizeof(listint_t));
-    if (!new || !head)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (idx == 0)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
-
-    for (i = 0; temp && i < idx; i++)
-    {
-        if (i == idx - 1)
-        {
-            new->next = temp->next;
-            temp->next = new;
-            return (new);
-        }
-        else
-            temp = temp->next;
-    }
-
-    return (NULL);
+	while (head != NULL)
+	{
+		total += head->n;
+		head = head->next;
+	}
+	return (total);
 }
